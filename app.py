b080ec15e57f59c3802b37b9bcfdbd462a16a111
Kiet Diep <kiet.diep@email.ucr.edu>
2022-01-14 14:34:01 -0800

    updated to single stock tracking implementation

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class stock_db():
 
         self.id_dict = {}
         self.date_list = []
-        self.value_nested_list = []
+        self.value_nested_list = [] #reason nested list is in case of future implementation where i show more than one stock graphs
 
         try:
             client = MongoClient("mongodb+srv://kietddiep:yuzumelon221@kcluster.9670b.mongodb.net/stock_info?retryWrites=true&w=majority")
@@ -43,48 +43,63 @@ class stock_db():
     def date_to_list(self,stock_dict):
         for key in stock_dict:
             date = self.dt64_to_date(int(key))
+            #depending on interval change will need to adjust this based on usage
             date = date.year + (date.month / 12) + (date.day / 365)
             self.date_list.append(date)
+
+    def show_plot(self,ticker):
+        x_axis = self.date_list
+        y_axis = self.value_nested_list[0]
+        plt.plot(x_axis,y_axis)
+        plt.ylabel("Stock Price $USD")
+        plt.xlabel("Year")
+        plt.title(ticker)
+        plt.show()
         
         
 if __name__ == "__main__":
-    # while loop that continually calls on class functions
     s_db = stock_db()
     
     #Step 2: Get the ticker input and do a call to get the data on that stock
     #Step 3: Store the data into MongoDB for later retrieval 
-    while True:
-        ticker = input("Enter desired ticker or 'quit': ")
-        if ticker.lower() == "quit":
-            break
-        else:
-            ticker_obj = yf.Ticker(ticker)
-            ticker_json = ticker_obj.history(period="10y",interval="1d").dropna().to_json()
-            s_db.insert_one(ticker_json,ticker)
+    
+    # ---------------------- Do not need for now as implementation is only for one stock -------------------------
+    # ------------------------------ Will adjust later when showing subplots ------------------------------------- 
+    # while True:
+    #     ticker = input("Enter desired ticker, 'c' to continue, or 'quit': ")
+    #     if ticker.lower() == "quit" or ticker.lower() == 'c':
+    #         break
+    #     else:
+    #         ticker_obj = yf.Ticker(ticker)
+    #         ticker_json = ticker_obj.history(period="10y",interval="1d").dropna().to_json()
+    #         s_db.insert_one(ticker_json,ticker)
+    # -------------------------------------------------------------------------------------------------------------
+    ticker = input("Enter desired ticker to begin: ")
+    ticker_obj = yf.Ticker(ticker)
+    ticker_json = ticker_obj.history(period="10y",interval="1d").dropna().to_json()
+    s_db.insert_one(ticker_json,ticker)
 
     #Step 4 use the ticker_id dictionary to get data that we wish to see
     #4.5 with id_dict, use keys to index values and values to push into obj_id
-    for key in s_db.id_dict:
-        obj_id = s_db.id_dict[key]
-        stock_dict = s_db.myCollection.find_one({"_id": obj_id})['Open'] #returns a dictionary with nested dictionaries
-        s_db.vdict_to_list(stock_dict) 
+    if ticker != "quit":    
+        for key in s_db.id_dict:
+            obj_id = s_db.id_dict[key]
+            stock_dict = s_db.myCollection.find_one({"_id": obj_id})['Open'] #returns a dictionary with nested dictionaries
+            s_db.vdict_to_list(stock_dict) 
 
-        if s_db.date_list:
-            break
-        else:
-            s_db.date_to_list(stock_dict)
-        
-    #Step 5: Display the values in matplotlib
-    y_axis = s_db.date_list
-    x_axis = s_db.value_nested_list[0]
-    plt.plot(y_axis,x_axis)
-    plt.ylabel("Stock Price $USD")
-    plt.xlabel("Year")
-    # plt.title("TWTR")
-    plt.show()
-
+            if s_db.date_list:
+                break
+            else:
+                s_db.date_to_list(stock_dict)
+            
+        s_db.show_plot(ticker)
+        del_col = s_db.myCollection.delete_many({})
     print("Have a nice day")
-    del_col = s_db.myCollection.delete_many({})
+    
 
 
 
+    #Steps to be done:
+    # - Input to choose from open, close, high, low
+    # - Input period
+    # - Input interval
